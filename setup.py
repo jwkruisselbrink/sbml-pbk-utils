079b031f94aaab7b3072cd925ee7696f96bda837
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-09-04 17:04:09 +0200

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='sbmlpbkutils',
-    version='0.0.1',
-    author='Johannes Kruisselbrink',
-    author_email='jwkruisselbrink@gmail.com',
-    description='Utils for FAIR PBK model development with SBML.',
-    long_description=long_description,
-    long_description_content_type ="text/markdown",
-    url='https://github.com/jwkruisselbrink/sbml-pbk-utils',
-    license='MIT',
-    packages=['sbmlpbkutils'],
-    install_requires=[
-        'antimony>=2.15.0',
-        'numpy>=1.26.4',
-        'python-libsbml>=5.20.2',
-        'sbmlutils>=0.9.0',
-        'tellurium>=2.2.10'
-    ],
-    keywords=['pbk','sbml','fair']
-)
+setup()
