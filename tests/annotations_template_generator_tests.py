de8e71e125d21b48d8c32af78c1b3a5ba8026c83
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-09-18 14:04:56 +0200

    Update annotations template generator test to write outputs

--- tests/annotations_template_generator_tests.py
+++ tests/annotations_template_generator_tests.py
@@ -2,10 +2,17 @@ import unittest
 import sys
 import os
 import libsbml as ls
+from pathlib import Path
 from sbmlpbkutils import AnnotationsTemplateGenerator
 sys.path.append('../sbmlpbkutils/')
 
+__test_outputs_path__ = './tests/__testoutputs__'
+
 class AnnotationsTemplateGeneratorTests(unittest.TestCase):
+
+    def setUp(self):
+        from pathlib import Path
+        Path(__test_outputs_path__).mkdir(parents=True, exist_ok=True)
 
     @classmethod
     def setUpClass(cls):
@@ -22,6 +29,9 @@ class AnnotationsTemplateGeneratorTests(unittest.TestCase):
             model = document.getModel()
             df = infosExporter.generate(model)
             self.assertEqual(df.ndim, 2)
+            sbml_basename = os.path.basename(sbml_file)
+            csv_file = os.path.join(__test_outputs_path__, Path(sbml_basename).with_suffix('.csv'))
+            df.to_csv(csv_file, index=False)
 
 if __name__ == '__main__':
     unittest.main()
