b5ee2b7a2009a685e9975cd01332fe3dc187d9ee
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-09-29 12:09:15 +0200

    Update/complete qualifier definitions

--- sbmlpbkutils/qualifier_definitions.py
+++ sbmlpbkutils/qualifier_definitions.py
@@ -7,8 +7,98 @@ QualifierDefinitions = [
         "type": ls.MODEL_QUALIFIER
     },
     {
+        "id": "BQM_IS_DESCRIBED_BY",
+        "qualifier": ls.BQM_IS_DESCRIBED_BY,
+        "type": ls.MODEL_QUALIFIER
+    },
+    {
+        "id": "BQM_IS_DERIVED_FROM",
+        "qualifier": ls.BQM_IS_DERIVED_FROM,
+        "type": ls.MODEL_QUALIFIER
+    },
+    {
+        "id": "BQM_IS_INSTANCE_OF",
+        "qualifier": ls.BQM_IS_INSTANCE_OF,
+        "type": ls.MODEL_QUALIFIER
+    },
+    {
+        "id": "BQM_HAS_INSTANCE",
+        "qualifier": ls.BQM_HAS_INSTANCE,
+        "type": ls.MODEL_QUALIFIER
+    },
+    {
+        "id": "BQM_UNKNOWN",
+        "qualifier": ls.BQM_UNKNOWN,
+        "type": ls.MODEL_QUALIFIER
+    },
+    {
         "id": "BQB_IS",
         "qualifier": ls.BQB_IS,
         "type": ls.BIOLOGICAL_QUALIFIER
     },
+    {
+        "id": "BQB_HAS_PART",
+        "qualifier": ls.BQB_HAS_PART,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_PART_OF",
+        "qualifier": ls.BQB_IS_PART_OF,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_VERSION_OF",
+        "qualifier": ls.BQB_IS_VERSION_OF,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_HAS_VERSION",
+        "qualifier": ls.BQB_HAS_VERSION,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_HOMOLOG_TO",
+        "qualifier": ls.BQB_IS_HOMOLOG_TO,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_DESCRIBED_BY",
+        "qualifier": ls.BQB_IS_DESCRIBED_BY,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_ENCODED_BY",
+        "qualifier": ls.BQB_IS_ENCODED_BY,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_ENCODES",
+        "qualifier": ls.BQB_ENCODES,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_OCCURS_IN",
+        "qualifier": ls.BQB_OCCURS_IN,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_HAS_PROPERTY",
+        "qualifier": ls.BQB_HAS_PROPERTY,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_IS_PROPERTY_OF",
+        "qualifier": ls.BQB_IS_PROPERTY_OF,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_HAS_TAXON",
+        "qualifier": ls.BQB_HAS_TAXON,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    },
+    {
+        "id": "BQB_UNKNOWN",
+        "qualifier": ls.BQB_UNKNOWN,
+        "type": ls.BIOLOGICAL_QUALIFIER
+    }
 ]
